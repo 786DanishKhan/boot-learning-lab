3f13cdda969751042f50a9781d2c365327b9d889
786DanishKhan <danish091098@gmail.com>
2025-12-08 22:12:24 +0530

    @service class and business logic

--- src/main/java/com/boot_learning_lab/controller/StudentController.java
+++ src/main/java/com/boot_learning_lab/controller/StudentController.java
@@ -1,52 +1,53 @@
 package com.boot_learning_lab.controller;
 
 import com.boot_learning_lab.model.Student;
-import org.springframework.web.bind.annotation.*;
+import com.boot_learning_lab.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 public class StudentController {
-    List<Student> students = new ArrayList<>();
+    @Autowired
+    StudentService studentService;
 
-    @GetMapping("/student")
+
+    @GetMapping("/students")
     public List<Student> student() {
-        //Student student = new Student("Danish", "Engineering");
-        // return student;
-        return students;
+        return studentService.getAllStudent();
     }
 
     @PostMapping("/add-student")
-    public Student addStudent(@RequestBody Student student) {
-        students.add(student);
-        return student;
+    public Student addStudent(@RequestBody Student obj) {
+        studentService.addStudent(obj);
+        return obj;
     }
 
-    @DeleteMapping("/delete-student/{name}")
-    public void deleteStudent(@PathVariable String name){
-        for(Student s :students ){
-            if(s.getName().equals(name)){
+    /* *//*  @DeleteMapping("/delete-student/{name}")
+    public void deleteStudent(@PathVariable String name) {
+        for (Student s : students) {
+            if (s.getName().equals(name)) {
                 students.remove(s);
             }
         }
     }
+
     @PutMapping("/update-student")
-    public void updateStudent(@RequestBody Student student){
-        for (Student s : students){
-            if(s.getName().equals(student.getName())){
+    public void updateStudent(@RequestBody Student student) {
+        for (Student s : students) {
+            if (s.getName().equals(student.getName())) {
                 s.setCourse(student.getCourse());
             }
-        }
-
-    }
+        }*//*
 
 
+    }*/
 
 
-
-
-
-    }
+}
 
--- src/main/java/com/boot_learning_lab/service/StudentService.java
+++ src/main/java/com/boot_learning_lab/service/StudentService.java
@@ -1,8 +1,24 @@
 package com.boot_learning_lab.service;
 
+import com.boot_learning_lab.model.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
+    List<Student> students = new ArrayList<>();
 
+    public List<Student> getAllStudent() {
+        return students;
+    }
+
+    public void addStudent(Student obj) {
+        students.add(obj);
+    }
+
+    public void deleteStudent() {
+
+    }
 }
